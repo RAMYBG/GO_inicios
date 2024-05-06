d5d02825917f2c05f05c94433a4f2c82f90c38a8
Ramiro <ramiro.2020110013@metropoli.edu.mx>
2024-05-06 17:37:36 -0600

    Declaracion de variable con iota

--- src/hola_mundo/hola_mundo.go
+++ src/hola_mundo/hola_mundo.go
@@ -8,11 +8,29 @@ import (
 
 // Declaracion de constante
 const pi float32 = 3.1416
+const (
+	x = 100
+	y = 0b1010 //binario
+	z = 0o12   //Octal
+	w = 0xFF   //Hexadecimal
+)
+
+// Con iota va incrementando de uno en uno y inicia en 0
+const (
+	Domingo = iota + 1
+	Lunes
+	Martes
+	Miercoles
+	Jueves
+	Viernes
+	Sabado
+)
 
 func main() {
 	fmt.Println("Hola mundo")
 	fmt.Println(quote.Hello())
 	fmt.Println(pi)
+	fmt.Println(y, x, w, z)
 	var firstName, lastName, age = "Alex", "Roe", 27
 	println(firstName, lastName, age)
 }
