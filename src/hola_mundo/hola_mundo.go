ae98e7dad8ed81d055d18164bafeb357a12d4031
Ramiro <ramiro.2020110013@metropoli.edu.mx>
2024-05-06 17:28:54 -0600

    Declaracion de constante

--- src/hola_mundo/hola_mundo.go
+++ src/hola_mundo/hola_mundo.go
@@ -6,10 +6,13 @@ import (
 	"rsc.io/quote"
 )
 
+// Declaracion de constante
+const pi float32 = 3.1416
+
 func main() {
 	fmt.Println("Hola mundo")
 	fmt.Println(quote.Hello())
-
+	fmt.Println(pi)
 	var firstName, lastName, age = "Alex", "Roe", 27
 	println(firstName, lastName, age)
 }
