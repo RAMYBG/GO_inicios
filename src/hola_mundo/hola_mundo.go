05da374ae4c941d898829b818e82332c01bf8e53
Ramiro <ramiro.2020110013@metropoli.edu.mx>
2024-05-06 18:17:47 -0600

    Valores predeterminados en variables

--- src/hola_mundo/hola_mundo.go
+++ src/hola_mundo/hola_mundo.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"math"
 
 	"rsc.io/quote"
 )
@@ -29,21 +28,14 @@ const (
 
 func main() {
 	fmt.Println("Hola mundo")
-	fmt.Println(quote.Hello())
-	fmt.Println(pi)
-	fmt.Println(y, x, w, z)
-	var firstName, lastName, age = "Alex", "Roe", 27
-	println(firstName, lastName, age)
-	fmt.Println(math.MinInt64, math.MaxInt64)   // Valores máximo y mínimo para int64
-	fullName := "Ramiro \t(alias \"Garcia\")\n" // Nombre y alias en formato tabulado
-	fmt.Println(fullName)
-
-	//TIpo de dato byte en ascii
-	var a byte = 'a'
-	fmt.Println(a) // Imprime el valor ASCII de 'a'
-	s := "hola"
-	fmt.Println(s[0]) // Imprime el valor ASCII de 'h'
-	// Uso de tipo de dato rune para almacenar un caracter Unicode
-	var r rune = '❤' // Imprime el unicode o ascii del corazon
-	fmt.Println(r)
+	fmt.Println(quote.Go())
+	//Valores predeterminados de cada variable
+	var (
+		defaultint    int
+		defaultUint   uint
+		defaultFloat  float32
+		defaultBool   bool
+		defaultString string
+	)
+	fmt.Println(defaultint, defaultUint, defaultFloat, defaultBool, defaultString)
 }
