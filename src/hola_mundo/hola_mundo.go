654bb65239d8c8f7616f8b8211159a3bf9a3c200
Ramiro <ramiro.2020110013@metropoli.edu.mx>
2024-05-06 21:00:46 -0600

    Tipos de prints y into datos desde consola

--- src/hola_mundo/hola_mundo.go
+++ src/hola_mundo/hola_mundo.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 
 	"rsc.io/quote"
 )
@@ -28,14 +29,36 @@ const (
 
 func main() {
 	fmt.Println("Hola mundo")
-	fmt.Println(quote.Go())
-	//Valores predeterminados de cada variable
-	var (
-		defaultint    int
-		defaultUint   uint
-		defaultFloat  float32
-		defaultBool   bool
-		defaultString string
-	)
-	fmt.Println(defaultint, defaultUint, defaultFloat, defaultBool, defaultString)
+	fmt.Println(quote.Hello())
+	var interger16 int16 = 50
+	var integer32 int32 = 100
+	//Para sumar valores hay que comvertirlos en el mismo formato int32 o int16
+	fmt.Println(interger16 + int16(integer32))
+
+	//Cuando se quiere convertir un string a un entero se necesita de un paquete
+	s := "100"
+	//i, _ := strconv.Atoi(s) //Convierte el string en entero _ espara no utilizar el error
+
+	n := 42
+	s = strconv.Itoa(n)
+	fmt.Println(s + s)
+	name := "Ramiro"
+	age := 23
+	//TIpos de Print con formato
+	fmt.Printf("Hola, me llamo %s y tengo %d años.\n", name, age)
+	greeting := fmt.Sprintf("Hola, me llamo %s y tengo %d años.\n", name, age)
+	fmt.Println(greeting)
+	///////
+	/////////
+	var name1 string
+	var name2 string
+	var age1 int
+	//Para insertar datos desde la co sola
+	fmt.Println("Ingrese su nombre:")
+	fmt.Scanln(&name1, &name2)
+	fmt.Println("Ingrese su edad:")
+	fmt.Scanln(&age1)
+	fmt.Printf("Hola, me llamo %s %sy tengo %d años.\n", name1, name2, age1)
+	//Para regresar el tipo de dato
+	fmt.Printf("El tipo de name es: %T\n", name1)
 }
