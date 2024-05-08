d7ace8a3c45847298cb3c620a50e7d9e0252c4c5
Ramiro <ramiro.2020110013@metropoli.edu.mx>
2024-05-08 00:06:17 -0600

    Uso de errores, aticipar errores y declaracion de errores

--- bucles/EstructuradeDatos/estructuradeDatos.go
+++ bucles/EstructuradeDatos/estructuradeDatos.go
@@ -2,29 +2,54 @@ package main
 
 import "fmt"
 
-type Persona struct {
-	nombre string
-	edad   int
-	correo string
+type Tarea struct {
+	nombre     string
+	desc       string
+	completado bool
 }
 
-func (s *Persona) diHola() { //Se utiliza el puntero de la estructura Persona s es la instancia probicional
-	fmt.Println("Hola, mi nombre es", s.nombre)
+type ListaTareas struct {
+	tareas []Tarea
 }
 
-//Con el puntero se hace referencia a la memoria
+// Metodo para agregar tarea
+func (l *ListaTareas) agregarTarea(t Tarea) {
+	l.tareas = append(l.tareas, t)
+}
+
+// Metodo para marcar como completado
+func (l *ListaTareas) marcarCompletado(index int) {
+	l.tareas[index].completado = true
+}
+
+// metodo para editar tarea
+func (l *ListaTareas) editarTarea(index int, t Tarea) {
+	l.tareas[index] = t
+}
+
+// Metodo para eliminar tarea
+func (l *ListaTareas) eliminarTarea(index int) {
+	l.tareas = append(l.tareas[:index], l.tareas[index+1:]...)
+}
 func main() {
-	s := Persona{"Ramiro", 23, "correo@gmail.com"}
-	s.diHola()
-	var x int = 10
-	var p *int = &x
-	fmt.Println(p)
-	fmt.Println(&x)
-	fmt.Println(*p)
-	editar(&x)
-	fmt.Println(x)
-}
-
-func editar(x *int) {
-	*x = 20
+	//Instancia de lista d etareas
+	lista := ListaTareas{}
+	for {
+		var opcion int
+		fmt.Println("Selecione una opcion:\n",
+			"1. Agregar tarea\n",
+			"2. Marcar tarea como completada\n",
+			"3. Editar tarea\n",
+			"4. Eliminar tarea\n",
+			"5. Salir")
+		fmt.Println("Ingrese la opcion: ")
+		fmt.Scanln(&opcion)
+	}
+	//Listar todas las tareas
+	fmt.Println("Lista de tareas")
+	fmt.Println("===============================")
+	for i, t := range lista.tareas {
+		fmt.Printf("%d. %s -%s - Completado: %t\n", i, t.nombre, t.desc, t.completado)
+	}
+	fmt.Println("================================")
 }
