018a650cec3037bbb69f4e42aedfa5dbf85b7471
Ramiro <ramiro.2020110013@metropoli.edu.mx>
2024-05-07 20:00:40 -0600

    Declaracion de mapas y usos

--- bucles/EstructuradeDatos/estructuradeDatos.go
+++ bucles/EstructuradeDatos/estructuradeDatos.go
@@ -4,24 +4,24 @@ import "fmt"
 
 //Rebanadas En go se pueden agregar y quitar elementos // Arreglos no se puede
 func main() {
-	var a []int //Hasta aqui declaramos una rebanada
-	diasSemana := []string{"Domingo", "Sabado", "Lunes",
-		"Martes"} //Rebanada
-	diaRebanada := diasSemana[0:2] //De donde a donde de diaSemana//Trozo
-	fmt.Println(a, diaRebanada)
-	//Para saber cuantos elementos tiene es con len(variable)
-	//Para saber cuantos elementos puede contener es con cap si es una rebanada de otra entoces dice
-	//de la rebanada tiene 2 pero del total tiene 7 entoces su capacidad es 7
-	//make para declarar una rebanada
-	nombres := make([]string, 5, 10) //el 5 es el numero de elementos que va almacenar inicialmente y 10 la capacidad
-	//Para asignarle valor a la rebana primero es de nombre[0]=Ramiro esto es hasta el indice 4 y despues de eso se debe utilizar append
-	nombres[0] = "ramiro"
-	fmt.Println(nombres)
-	//Fincion copy copia de una rebanada a otra
-	rebanada1 := []int{1, 2, 3, 4, 5}
-	rebanada2 := make([]int, 5)
-	copy(rebanada2, rebanada1)              //Copia de rebanada 1 a la 2
-	fmt.Println(copy(rebanada2, rebanada1)) //Imprime la cantidad de elementos que se copiaron
-	fmt.Println(rebanada1)
-	fmt.Println(rebanada2)
+	colors := map[string]string{
+		"rojo":  "#FF0000",
+		"verde": "#00F00",
+		"azul":  "#0000FF",
+	}
+	fmt.Println(colors["rojo"]) //Imprime el valor hexadecimal
+	colors["negro"] = "000000"
+	fmt.Println(colors)
+	if valor, ok := colors["blanco"]; ok { //Se decalra la variable valor y tambien se hace la autentificacion y despues se evalua el ok =verdadero en el if
+		fmt.Println(valor)
+	} else {
+		fmt.Println("No existe la clave")
+	}
+	delete(colors, "azul") //Elimina el azul dentro del mapa
+	fmt.Println(colors)
+
+	//Iterar todos los valores del map
+	for clave, valor := range colors { //Clave nombre, valor valor ...map[string]string
+		fmt.Printf("Clave: %s, valor: %s\n", clave, valor) //Formato
+	}
 }
