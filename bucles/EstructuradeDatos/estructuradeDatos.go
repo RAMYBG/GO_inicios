b704f1e062eb3e54a86f051b605a153892fe5fc1
Ramiro <ramiro.2020110013@metropoli.edu.mx>
2024-05-07 20:27:55 -0600

    Declaracion de mapas y usos

--- bucles/EstructuradeDatos/estructuradeDatos.go
+++ bucles/EstructuradeDatos/estructuradeDatos.go
@@ -2,26 +2,20 @@ package main
 
 import "fmt"
 
-//Rebanadas En go se pueden agregar y quitar elementos // Arreglos no se puede
-func main() {
-	colors := map[string]string{
-		"rojo":  "#FF0000",
-		"verde": "#00F00",
-		"azul":  "#0000FF",
-	}
-	fmt.Println(colors["rojo"]) //Imprime el valor hexadecimal
-	colors["negro"] = "000000"
-	fmt.Println(colors)
-	if valor, ok := colors["blanco"]; ok { //Se decalra la variable valor y tambien se hace la autentificacion y despues se evalua el ok =verdadero en el if
-		fmt.Println(valor)
-	} else {
-		fmt.Println("No existe la clave")
-	}
-	delete(colors, "azul") //Elimina el azul dentro del mapa
-	fmt.Println(colors)
+type Persona struct {
+	nombre string
+	edad   int
+	correo string
+}
 
-	//Iterar todos los valores del map
-	for clave, valor := range colors { //Clave nombre, valor valor ...map[string]string
-		fmt.Printf("Clave: %s, valor: %s\n", clave, valor) //Formato
-	}
+// Estructura de datos
+func main() {
+	var persona1 Persona //Se instancia
+	persona1.nombre = "Ramiro"
+	persona1.edad = 23
+	persona1.correo = "ramiro@gamil.com"
+	fmt.Println(persona1)
+	//Otra forma de instanciar
+	persona2 := Persona{"ramiro", 23, "Correo@gmail.com"}
+	fmt.Println(persona2)
 }
