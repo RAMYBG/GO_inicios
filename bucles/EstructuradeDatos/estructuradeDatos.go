f36638a9f593795f2b21a0a376717b06c845fe47
Ramiro <ramiro.2020110013@metropoli.edu.mx>
2024-05-08 17:57:56 -0600

    Proyecto estructura de datos

--- bucles/EstructuradeDatos/estructuradeDatos.go
+++ bucles/EstructuradeDatos/estructuradeDatos.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 type Tarea struct {
 	nombre     string
@@ -34,6 +38,8 @@ func (l *ListaTareas) eliminarTarea(index int) {
 func main() {
 	//Instancia de lista d etareas
 	lista := ListaTareas{}
+	leer := bufio.NewReader(os.Stdin) //Esto es para leer la entrada de varios caracteres
+	//en el scanln
 	for {
 		var opcion int
 		fmt.Println("Selecione una opcion:\n",
@@ -44,12 +50,48 @@ func main() {
 			"5. Salir")
 		fmt.Println("Ingrese la opcion: ")
 		fmt.Scanln(&opcion)
+		switch opcion {
+		case 1:
+			var t Tarea
+			fmt.Print("Ingresa el nombre de la tarea:")
+			t.nombre, _ = leer.ReadString('\n')
+			fmt.Print("Ingrese la descripcion de la tarea: ")
+			t.desc, _ = leer.ReadString('\n')
+			lista.agregarTarea(t)
+			fmt.Println("Tarea agregada correctamente.")
+		case 2:
+			var index int
+			fmt.Print("Ingrese el indice de la tarea a marcar como completada: ")
+			fmt.Scanln(&index)
+			lista.marcarCompletado(index)
+			fmt.Println("Tarea marcada como completada")
+		case 3:
+			var index int
+			var t Tarea
+			fmt.Print("Ingrese el indice de la tarea a actuaiizar: ")
+			fmt.Scanln(&index)
+			t.desc, _ = leer.ReadString('\n')
+			lista.editarTarea(index, t)
+			fmt.Println("Tarea actualizada correctamente.")
+		case 4:
+			var index int
+			fmt.Print("Ingrese el indice de la tarea a eliminar: ")
+			fmt.Scanln(&index)
+			lista.eliminarTarea(index)
+			fmt.Println("Tarea eliminada correctamente.")
+		case 5:
+			fmt.Print("Saliendo del programa...")
+			return
+		default:
+			fmt.Println("Opcion no valida")
+			//Listar todas las tareas
+			fmt.Println("Lista de tareas")
+			fmt.Println("===============================")
+			for i, t := range lista.tareas {
+				fmt.Printf("%d. %s -%s - Completado: %t\n", i, t.nombre, t.desc, t.completado)
+			}
+			fmt.Println("================================")
+		}
 	}
-	//Listar todas las tareas
-	fmt.Println("Lista de tareas")
-	fmt.Println("===============================")
-	for i, t := range lista.tareas {
-		fmt.Printf("%d. %s -%s - Completado: %t\n", i, t.nombre, t.desc, t.completado)
-	}
-	fmt.Println("================================")
+
 }
