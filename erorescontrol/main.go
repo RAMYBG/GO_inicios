c570cdd7480a7b9b136301e778cfcf46b3319cab
Ramiro <ramiro.2020110013@metropoli.edu.mx>
2024-05-08 19:07:40 -0600

    Registro de errores

--- erorescontrol/main.go
+++ erorescontrol/main.go
@@ -1,25 +1,12 @@
 package main
 
-import "fmt"
-
-func divide(dividiendo, divisor int) {
-	//Funcio anonima
-	defer func() {
-		if r := recover(); r != nil {
-			fmt.Println(r)
-		}
-	}()
-	validateZero(divisor)
-	fmt.Println(dividiendo / divisor)
-}
-func validateZero(divisor int) { //Se puede quitar y el sistema arroja el error
-	if divisor == 0 {
-		//Se utiliza para un panico
-		panic("no se puede dividir por cero")
-	}
-}
+import "log"
 
 func main() {
-	divide(100, 10)
-	divide(30, 0)
+	log.SetPrefix("main: ")                         //Prefijo para las lineas siguientes
+	log.Print("Este es un mesaje de registro")      //Inprime el tiempo en que ocurio
+	log.Println("Este es otro mensaje de registro") //Hace lo mismo que el anterior basicamente
+	//log.Fatal("Este es otro mensaje de registro")   // Se ejecuta y detiene la ejecucion  el programa
+	log.Panic("Este es otro mensaje de registro") //Nos muestra una pila de errores y registro y tambien termina la ejecucion
+
 }
