1fe513fbff0656383ebb4c77e57fb80d4e9c23be
Ramiro <ramiro.2020110013@metropoli.edu.mx>
2024-05-09 20:14:28 -0600

    Se agrego otra funcion para que de un slice de nombres se tome y de los mensaje aleatorios se generen mensajes para cada individuo

--- Modulo_Paquetes/greetings.go
+++ Modulo_Paquetes/greetings.go
@@ -7,6 +7,7 @@ import (
 )
 
 // Hello devuelve un saludo para la persona especificada
+
 func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("Nombre vacio") //Regresa un string vacio  un error
@@ -14,6 +15,18 @@ func Hello(name string) (string, error) {
 	//Hello devuelve un saludo para la persona especificada
 	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil //Regresa el mensaje y el error
+}
+func Hellos(names []string) (map[string]string, error) {
+	messages := make(map[string]string)
+
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		messages[name] = message
+	}
+	return messages, nil
 }
 
 func randomFormat() string {
--- hello/main.go
+++ hello/main.go
@@ -11,9 +11,17 @@ func main() {
 	log.SetPrefix("greetings") //prefijo en los mensaje de registro
 	log.SetFlags(0)            //Bandera de formato en 0// No se mostrar la fecha y hora
 
+	names := []string{
+		"Ramiro",
+		"Juan",
+		"Pedro",
+	}
+	messages, err := greetings.Hellos(names)
+
 	message, err := greetings.Hello("Ramiro") //Aqui se manea el error si se envia en vacio genera un error
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(message)
+	fmt.Println(messages)
 }
