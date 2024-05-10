1a5db92e1636931044f8cf01c176ee27a347649e
Ramiro <ramiro.2020110013@metropoli.edu.mx>
2024-05-10 00:24:58 -0600

    POO Objetos Publicos y Privados (Privados y Metodos(Funciones))

--- POO/librery/book/book.go
+++ POO/librery/book/book.go
@@ -5,22 +5,31 @@ import "fmt"
 //Atributos del Libro
 //Modelo de un objeto
 type Book struct {
-	Title  string
-	Author string
-	Pages  int
+	title  string
+	author string
+	pages  int
 }
 
 //Constructor simulacion
 func NewBook(title string, author string, pages int) *Book {
 	return &Book{
-		Title:  title,
-		Author: author,
-		Pages:  pages,
+		title:  title,
+		author: author,
+		pages:  pages,
 	}
 }
 
 //Metodo
 func (b *Book) PrintInfo() { //Se envia la estructura pero se envia como puntero
-	fmt.Printf("Title: %s\nAutor: %s\nPage: %d\n", b.Title, b.Author, b.Pages)
+	fmt.Printf("Title: %s\nAutor: %s\nPage: %d\n", b.title, b.author, b.pages)
 
+}
+
+//Obtener y Modificar
+func (b *Book) SetTitle(title string) {
+	b.title = title
+}
+
+func (b *Book) GetTitle() string {
+	return b.title
 }
--- POO/librery/main.go
+++ POO/librery/main.go
@@ -1,17 +1,17 @@
 package main
 
-import "librery/book"
+import (
+	"fmt"
+	"librery/book"
+)
 
 func main() {
 	//Primera forma de crear un objeto sencillo pero mucha linea de codigo
-	var myBook = book.Book{ //Se instancia
-		Title:  "The Go Programming Language",
-		Author: "John W. Johnson",
-		Pages:  23,
-	}
-	myBook.PrintInfo() //Se utiliza el metodo
 	//Forma dos instanciar y utlizar objeto
 	myBook2 := book.NewBook("Nombre de libro", "Autor del libro", 302)
 	myBook2.PrintInfo()
+
+	myBook2.SetTitle("Edicion Especial")
+	fmt.Println(myBook2.GetTitle())
 
 }
