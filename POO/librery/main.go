99d5f817c3112c59087d9530494cb5861e232116
Ramiro <ramiro.2020110013@metropoli.edu.mx>
2024-05-10 18:48:12 -0600

    Interface uso en el polimorfismo

--- POO/librery/main.go
+++ POO/librery/main.go
@@ -1,32 +1,36 @@
 package main
 
 import (
-	"fmt"
-	"librery/book"
+	"librery/animal"
 )
 
 func main() {
 	//Primera forma de crear un objeto sencillo pero mucha linea de codigo
 	//Forma dos instanciar y utlizar objeto
-	myBook2 := book.NewBook("Nombre de libro", "Autor del libro", 302)
-	myBook2.PrintInfo()
+	//myBook2 := book.NewBook("Nombre de libro", "Autor del libro", 302)
+	//myBook2.PrintInfo()
 
-	myBook2.SetTitle("Edicion Especial")
-	fmt.Println(myBook2.GetTitle())
-	myBook2.PrintInfo()
+	//myBook2.SetTitle("Edicion Especial")
+	//fmt.Println(myBook2.GetTitle())
+	//myBook2.PrintInfo()
 
-	myTextBook := book.NewTextBook("Comunicacion",
-		"jaime Gamarra", 263, "Santillana SAC", "Secundaria")
-	myTextBook.PrintInfo()
+	//myTextBook := book.NewTextBook("Comunicacion",
+	//	"jaime Gamarra", 263, "Santillana SAC", "Secundaria")
+	//myTextBook.PrintInfo()
 
-	fmt.Print("==========================\n")
-	fmt.Print("==========================\n")
-	fmt.Print("==========================\n")
-	myBook2.PrintInfo()
-	myTextBook.PrintInfo()
-	fmt.Print("==========================\n")
-	fmt.Print("==========================\n")
-	fmt.Print("==========================\n")
-	book.Print(myBook2)
-	book.Print(myTextBook)
+	//fmt.Print("==========================\n")
+	//fmt.Print("==========================\n")
+	//fmt.Print("==========================\n")
+	//myBook2.PrintInfo()
+	//myTextBook.PrintInfo()
+	//fmt.Print("==========================\n")
+	//fmt.Print("==========================\n")
+	//fmt.Print("==========================\n")
+	//book.Print(myBook2)
+	//book.Print(myTextBook)
+	miPerro := animal.Perro{Nombre: "Max"}
+	miGato := animal.Gato{Nombre: "Tom"}
+	animal.HacerSonido(&miPerro)
+	animal.HacerSonido(&miGato)
+
 }
