bf1cb7874739ef9f7050bb3c2a0bc52a4c8adf1b
Ramiro <ramiro.2020110013@metropoli.edu.mx>
2024-05-12 18:46:35 -0600

    Funciones Avanzadas donde recibe N cantidad de int y un string

--- Finciones_Avanzadas/main.go
+++ Finciones_Avanzadas/main.go
@@ -12,6 +12,13 @@ func suma(name string, nums ...int) int {
 	return total
 }
 
+func imprimirDatos(datos ...interface{}) { //Interface es para recibir dats sin discriminacion
+	for _, dato := range datos {
+		fmt.Printf("%T - %v\n", dato, dato)
+	}
+}
+
 func main() {
 	fmt.Println(suma("Ramiro", 102, 34, 52, 44))
+	imprimirDatos("Hola", 132, 123.45, true) //Imprime que tipo de datos es y recibe N cantidad de datos sin importar el tipo
 }
