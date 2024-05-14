b3427de6e1c32161848ed31307a743b4ff8198b4
Ramiro <ramiro.2020110013@metropoli.edu.mx>
2024-05-13 18:33:37 -0600

    Se utiliza modulo para las restricciones y interfaces para declarar un type interface {tipos de datos se puede usar union para el grupo}

--- Generic/main.go
+++ Generic/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	//Se utiliza el modulo de paquetes de GO
+	"golang.org/x/exp/constraints"
+)
 
 func PrintList(lsit ...interface{}) {
 	for _, value := range lsit {
@@ -8,14 +12,14 @@ func PrintList(lsit ...interface{}) {
 	}
 }
 
-//Hace lo mismo que interface{}
+// Hace lo mismo que interface{}
 func PrintList2(lsit ...any) {
 	for _, value := range lsit {
 		fmt.Println(value)
 	}
 }
 
-//Aproximacion
+// Aproximacion
 func Sum[T ~int | ~float64](nums ...T) T {
 	var total T
 	for _, num := range nums {
@@ -24,7 +28,7 @@ func Sum[T ~int | ~float64](nums ...T) T {
 	return total
 }
 
-//Restrinccion union de elementor
+// Restrinccion union de elementor
 func Sum2[T int | float64](nums ...T) T {
 	var total T
 	for _, num := range nums {
@@ -33,9 +37,32 @@ func Sum2[T int | float64](nums ...T) T {
 	return total
 }
 
-//Restrinccion albritrario
-//Restrinccion
+// Restrinccion albritrario
+// Restrinccion
 func Sum3[T int](nums ...T) T {
+	var total T
+	for _, num := range nums {
+		total += num
+	}
+	return total
+}
+
+type Numers interface {
+	~int | ~float64 | ~float32 | ~uint
+}
+
+// Restrinccion albritrario
+// Restrinccion
+func Sum4[T Numers](nums ...T) T {
+	var total T
+	for _, num := range nums {
+		total += num
+	}
+	return total
+}
+
+// Se utiliza el  modulo
+func Sum5[T constraints.Integer](nums ...T) T {
 	var total T
 	for _, num := range nums {
 		total += num
@@ -50,4 +77,6 @@ func main() {
 	PrintList(35, "World", 12)
 	//Imprime la lista de valores infinito
 	PrintList2("Hello", 1, "Go")
+	fmt.Printf("Suma: %d \n", Sum(1, 2, 41, 4))
+	fmt.Printf("Suma5: %d", Sum5(2, 2, 41, 4))
 }
