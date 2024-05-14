0597938fe04dc9deb878f9190e86d8fbd9e84138
Ramiro <ramiro.2020110013@metropoli.edu.mx>
2024-05-13 19:10:33 -0600

    Restricciones y operadores (checa si la letra esta en la lista y otro hace un filtro)

--- Generic/main.go
+++ Generic/main.go
@@ -70,7 +70,29 @@ func Sum5[T constraints.Integer](nums ...T) T {
 	return total
 }
 
+func Includes[T comparable](list []T, value T) bool {
+	for _, item := range list {
+		if item == value {
+			return true
+		}
+	}
+	return false
+}
+
+// Crea una funcion para filtrar utiliza la restrinccion de Oredered recibe una lista con la restriccion, recibe una funcion que recibe un entero o los dstos que este marcado la restriccion y regresa un bool
+func Filter[T constraints.Ordered](list []T, callback func(T) bool) []T {
+	resul := make([]T, 0, len(list)) //Crea una lista para guardar el filtro despues
+	for _, item := range list {
+		if callback(item) { //Si la funcion callback devuelve verdadero
+			resul = append(resul, item) //Se hace apppend a la lista resul
+		}
+	}
+	return resul
+}
+
 func main() {
+	lista1 := []string{"a", "b", "c"}
+	lista2 := []int{1, 2, 3, 4, 5}
 	//Imprime la lista de valores infinito
 	PrintList("Hello", "World", "Go")
 	//Imprime la lista de valores infinito
@@ -78,5 +100,13 @@ func main() {
 	//Imprime la lista de valores infinito
 	PrintList2("Hello", 1, "Go")
 	fmt.Printf("Suma: %d \n", Sum(1, 2, 41, 4))
-	fmt.Printf("Suma5: %d", Sum5(2, 2, 41, 4))
+	fmt.Printf("Suma5: %d\n", Sum5(2, 2, 41, 4))
+	fmt.Println(Includes(lista1, "g"))
+	fmt.Println(Includes(lista1, "a"))
+	fmt.Println(Includes(lista2, 1))
+	fmt.Println(Includes(lista2, 8))
+
+	//Comienza a crear la lista apartir de la condicion del return
+	fmt.Println(Filter(lista2, func(value int) bool { return value > 3 }))
+
 }
